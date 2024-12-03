081537e643f7f1aced4058811f7f47adeb483bc6
andre <73719598+andre-471@users.noreply.github.com>
2024-12-02 22:37:20 -0500

    completed day 02!!!!!!!!!!!!
    Took 1 hour 1 minute

--- src/Day02.java
+++ src/Day02.java
@@ -4,34 +4,91 @@ import java.util.stream.Collectors;
 
 public class Day02 {
     public static void main(String[] args) {
-        partOne();
+//        partOne();
+        partTwo();
     }
 
     public static void partOne() {
-        ArrayList<String> data = FileHelper.readWithFiles("input/test");
-        System.out.println(data);
+        ArrayList<String> data = FileHelper.readWithFiles("input/Day02");
 
-        ArrayList<int[]> parsedData = data.stream().
-                map(str -> Arrays.stream(str.split(" "))
-                        .mapToInt(Integer::valueOf)
-                        .toArray())
-                .collect(Collectors.toCollection(ArrayList::new));
+        int sum = (int) data.stream()
+                // Convert all strings into arrays
+                .map(str -> Arrays.stream(str.split(" ")).mapToInt(Integer::valueOf).toArray())
+                // Removes reports that are unsafe
+                .filter(array -> {
+                    int sign = (int) Math.signum(array[1] - array[0]); // No edge case of delta == 0 since delta == 0 -> false
 
+                    for (int i = 0; i < array.length - 1; i++) {
+                        int delta = array[i + 1] - array[i];
+                        int deltaSign = (int) Math.signum(delta);
 
+                        if (Math.abs(delta) > 3 || Math.abs(delta) < 1 || sign != deltaSign) {
+                            return false;
+                        }
+                    }
 
-        Array list<int[]> sum = data.stream()
+                    return true;
+                })
+                .count();
+
+        System.out.println(sum);
+
+    }
+
+    public static void partTwo() {
+        ArrayList<String> data = FileHelper.readWithFiles("input/Day02");
+//        System.out.println(data);
+
+        int sum = (int) data.stream()
+                // Convert all strings into arrays
                 .map(str -> Arrays.stream(str.split(" "))
                         .mapToInt(Integer::valueOf)
-                        .toArray())
-                .filter(array -> {
-int sign = (array[1] - array[0])/Math.abs(array[1] - array[0]);
-for (int i = 0; i < array.length; i++) {
-int change = array[i + 1] - array[i];
-int changeSign = change/Math.abs(change);
-if (Math.abs(change) > 3 || change
-.collect(Collectors.toCollection(ArrayList::new));
+                        .<ArrayList<Integer>>collect(ArrayList::new, ArrayList::add, ArrayList::addAll)
+                )
+                // Removes reports that are unsafe
+                .filter(arrayList -> {
+                    if (isSafe(arrayList)) return true;
 
+                    int removalIndex = 0;
+                    while (removalIndex < arrayList.size()) {
+                        ArrayList<Integer> arrayListRemoved = new ArrayList<>(arrayList);
+                        arrayListRemoved.remove(removalIndex);
 
+                        if (isSafe(arrayListRemoved)) return true;
+
+                        removalIndex++;
+                    }
+
+                    return false;
+                })
+                .count();
+
+        System.out.println(sum);
+
+    }
+
+    public static boolean isSafe(ArrayList<Integer> arrayList) {
+        int sign = (int) Math.signum(arrayList.get(1) - arrayList.get(0));
+        if (sign == 0) {
+            return false;
+        }
+
+        for (int i = 0, len = arrayList.size(); i < len - 1; i++) {
+            int delta = arrayList.get(i + 1) - arrayList.get(i);
+            boolean order;
+
+            if (sign == -1) {
+                order = delta <= -1 && delta >= -3;
+            } else {
+                order = delta >= 1 && delta <= 3;
+            }
+
+            if (!order) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
 }
