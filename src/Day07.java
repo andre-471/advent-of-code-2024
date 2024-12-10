544bb2bdf17498dec7e858cede8d474694dac5ce
andre <73719598+andre-471@users.noreply.github.com>
2024-12-09 23:25:26 -0500

    too tired x2

--- src/Day07.java
+++ src/Day07.java
@@ -3,27 +3,38 @@ import java.util.Arrays;
 
 public class Day07 {
     public static void main(String[] args) {
-
+        partOne();
     }
 
     public static void partOne() {
         ArrayList<String> data = FileHelper.read("input/test");
+        int sum = 0;
 
         for (String line : data) {
             String[] hi = line.split(": ");
             ArrayList<Integer> hi2 = Arrays.stream(hi[1].split(" ")).mapToInt(Integer::valueOf).collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
 
+            if (testNumber(Integer.parseInt(hi[0]), hi2)) {
+                sum += Integer.parseInt(hi[0]);
+            }
         }
+
+        System.out.println(sum);
     }
 
-    public static boolean testNumber(int base, int num) {
+    public static boolean testNumber(int base, ArrayList<Integer> nums) {
+        if (nums.isEmpty()) {
+            return base == 0;
+        }
+
+        int num = nums.removeFirst();
         int divided = base / num;
         int subtracted = base - num;
 
         if (base % num != 0) {
-            return testNumber(subtracted, num);
+            return testNumber(subtracted, new ArrayList<>(nums));
         } else {
-            return testNumber(divided, )
+            return testNumber(divided, new ArrayList<>(nums)) || testNumber(subtracted, new ArrayList<>(nums));
         }
     }
 
